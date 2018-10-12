9c264963fb6223ece96fa1cbf7c5a5ed5e9aa98f
kidoda <kdevb0x@gmail.com>
2018-10-12 13:45:34 -0700

    add Camera

--- camera2/camera2.go
+++ camera2/camera2.go
@@ -1,21 +1,18 @@
-package main // import "walkcam"
+package walkcam // import "walkcam"
 
-import (
-	"Java/android/hardware/Camera"
-	"Java/android/hardware/CameraInfo"
-	"Java/android/hardware/camera2/CameraCaptureSession"
-	"Java/android/hardware/camera2/CameraDevice"
-	"Java/android/hardware/camera2/CameraManager"
-	"Java/android/hardware/camera2/CameraMetadata"
-	"Java/android/hardware/camera2/CaptureFailure"
-	"Java/android/hardware/camera2/CaptureRequest"
-	"Java/android/hardware/camera2/CaptureResult"
-)
+import "Java/android/content/Context"
+import "Java/android/hardware/camera2/CameraCaptureSession"
+import "Java/android/hardware/camera2/CameraDevice"
+import "Java/android/hardware/camera2/CameraManager"
+import "Java/android/hardware/camera2/CameraMetadata"
+import "Java/android/hardware/camera2/CaptureFailure"
+import "Java/android/hardware/camera2/CaptureRequest"
+import "Java/android/hardware/camera2/CaptureResult"
+import "Java/android/view/WindowMan"
 import "Java/java/lang"
 
 import (
 	"runtime"
-	"unsafe"
 
 	"github.com/xlab/android-go/android"
 	"github.com/xlab/android-go/app"
